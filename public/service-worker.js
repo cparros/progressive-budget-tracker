b74d084fdc0af6bd87cea78121853ff722a43300
cparros <celparros@gmail.com>
2021-04-18 12:20:00 -0400

    moved manifest to correct folder created start to service worker

---
+++ public/service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/db.js", 
+  "/index.html",
+  "/index.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/manifest.webmanifest",
+  "/styles.css",
+  
+];
+
+// Installation can skip waiting life cycle
+self.addEventListener("install", function (evt) {
+ 
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+
+  self.skipWaiting();
+});
+
+// Activation
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
