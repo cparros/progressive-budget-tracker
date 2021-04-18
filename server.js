0a4f9136eb4c4294d6e3206a5a9688b2c9d1c4c4
cparros <celparros@gmail.com>
2021-04-18 13:10:24 -0400

    removed extra lines

--- server.js
+++ server.js
@@ -14,12 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
-
 // routes
 app.use(require("./routes/api.js"));
 
